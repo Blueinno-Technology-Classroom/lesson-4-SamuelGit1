da3beb939af8e9bc0c78921a537ca79a9f7e4e3b
Samuel Wong <wongsamuel12321@gmail.com>
2024-10-26 14:57:49 +0800

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+const { default: daisyui } = require("daisyui");
+
 module.exports = {
   content: ["./src/**/*.{html,js}", "./public/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: ["dark"],
+  },
+  plugins: [require("daisyui")],
 };
